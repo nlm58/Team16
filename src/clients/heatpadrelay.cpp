f5e2279eccd31e8c192a8f0516d0f054ed46f33f
wem56 <wem56@case.edu>
2026-03-14 22:00:52 -0400

    Add comments for relay pin setup and operation
    Add comments to clarify pin initialization and loop behavior.

--- src/clients/heatpadrelay.cpp
+++ src/clients/heatpadrelay.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 int relayPin = 1;
-
+// Initialize Pin on ESP32
 
 void setup() {
   pinMode(relayPin, OUTPUT);
+  // Set Pin as output
 }
 
 void loop() {
@@ -11,4 +12,5 @@ void loop() {
   delay(30000);
   digitalWrite(relayPin, LOW);
   delay(5000);
+  //Write high to pin for 30s and low for 5s
 }
